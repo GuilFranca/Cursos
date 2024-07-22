c47ca438f4ee97e7c93cef15f2aeb1be38398925
Guilherme França <131270776+GuilFranca@users.noreply.github.com>
2024-07-22 16:01:02 -0300

    Add files via upload

---
+++ Cursos/Python-Udemy/aulas/au40.py
@@ -0,0 +1,14 @@
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+
+# if entrada == 'E' and senha_digitada == senha_permitida:
+#     print('Entrar')
+# elif entrada == 'S':
+#     print('Sair')
+# else:
+#     print('Erro senha.')
+
+#Avaliação de curto circuito
+print(True and 0 and True)
