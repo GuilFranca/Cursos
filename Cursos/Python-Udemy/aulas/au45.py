b50fd753b55126cbab0fb4eb3bd0e03e71f9d394
Guilherme França <131270776+GuilFranca@users.noreply.github.com>
2024-08-01 23:03:29 -0300

    Add files via upload

---
+++ Cursos/Python-Udemy/aulas/au45.py
@@ -0,0 +1,14 @@
+var = 'ABC'
+
+# print(f'{var}')
+# print(f'{var: >10}')
+# print(f'{var: <10}.')
+# print(f'{var: ^10}.')
+
+# print(f'{1000.124238965963528:.2f}')
+
+# print(f'{123:+}')
+# print(f'{-123:-}')
+
+print(f'{1000.123456:,.2f}')
+
---
+++ Cursos/Python-Udemy/aulas/au46.py
@@ -0,0 +1,16 @@
+var = 'Olá mundo'
+
+# 012345678
+# Olá mundo
+#-987654321
+
+# print(var[ : : ])
+# print(var[4 : ])
+# print(var[0 : 8])
+# print(var[0 : 9])
+# print(var[ : : 2])
+# print(var[ : : -1])
+
+print(len(var))
+print(var[0 : len(var)])
+
